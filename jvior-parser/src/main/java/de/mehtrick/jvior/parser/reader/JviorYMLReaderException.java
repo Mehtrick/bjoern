1060d4026a764c93a5ee4ea003f5522489013acc
mehtrick <p.lauhof@gmail.com>
2018-09-02 19:05:53 +0200

    published new Jvior Release

--- jvior-parser/src/main/java/de/mehtrick/jvior/parser/reader/JviorYMLReaderException.java
+++ jvior-parser/src/main/java/de/mehtrick/jvior/parser/reader/JviorYMLReaderException.java
@@ -7,11 +7,6 @@ public class JviorYMLReaderException extends RuntimeException {
 	 */
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @param path
-	 * @param message
-	 * @param cause
-	 */
 	public JviorYMLReaderException(String path, Throwable cause) {
 		super(String.format("The File under %s could not be read. " + cause.getMessage(), path), cause);
 	}
