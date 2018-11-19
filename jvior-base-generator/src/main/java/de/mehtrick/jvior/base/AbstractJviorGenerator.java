e2291b71d15bce3cc5aa92d0d801dad2033bbdbb
Patrick <p.lauhof@gmail.com>
2018-11-19 15:11:22 +0100

    #19 empty files wont cause error

--- jvior-base-generator/src/main/java/de/mehtrick/jvior/base/AbstractJviorGenerator.java
+++ jvior-base-generator/src/main/java/de/mehtrick/jvior/base/AbstractJviorGenerator.java
@@ -6,11 +6,6 @@ import java.io.FileNotFoundException;
 public abstract class AbstractJviorGenerator {
 	protected static File[] getFilesFromFolder(String folder) throws FileNotFoundException {
 		File file = new File(folder);
-		File[] ymlFiles = file.listFiles(new JviorFileNameFilter());
-		if (ymlFiles.length == 0) {
-			throw new FileNotFoundException("No yaml files Found in Folder " + folder
-					+ ". The file has to end with .yml or .yaml to be detected");
-		}
 		return file.listFiles(new JviorFileNameFilter());
 	}
 }
