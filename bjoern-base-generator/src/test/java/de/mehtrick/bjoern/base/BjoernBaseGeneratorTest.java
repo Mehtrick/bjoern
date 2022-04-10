7916db3f01f8d928e3488e2d6f31f4d021497b10
Patrick <p.lauhof@gmail.com>
2022-04-10 11:03:13 +0200

    Dependency Updates

--- bjoern-base-generator/src/test/java/de/mehtrick/bjoern/base/BjoernBaseGeneratorTest.java
+++ bjoern-base-generator/src/test/java/de/mehtrick/bjoern/base/BjoernBaseGeneratorTest.java
@@ -2,16 +2,22 @@ package de.mehtrick.bjoern.base;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.util.Arrays;
 
-class AbstractBjoernGeneratorTest {
+class BjoernBaseGeneratorTest {
+
+    private static final Logger log = LoggerFactory.getLogger(BjoernBaseGeneratorTest.class);
 
     @Test
     void correctListFiles() {
         //when
         File[] bjoernFiles = AbstractBjoernGenerator.getFilesFromFolder("src/test/resources/AbstractBjoernGeneratorTest");
         //then
+        Arrays.stream(bjoernFiles).forEach(file -> log.info(file.getName()));
         Assertions.assertThat(bjoernFiles).hasSize(1);
     }
 
