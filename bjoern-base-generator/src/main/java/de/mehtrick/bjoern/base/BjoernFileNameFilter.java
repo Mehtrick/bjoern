c13f010f587f24d7cff0767efd35e3e4590155d9
Patrick <p.lauhof@gmail.com>
2022-04-15 14:19:58 +0200

    Dependency Updates

--- bjoern-base-generator/src/main/java/de/mehtrick/bjoern/base/BjoernFileNameFilter.java
+++ bjoern-base-generator/src/main/java/de/mehtrick/bjoern/base/BjoernFileNameFilter.java
@@ -1,6 +1,8 @@
 package de.mehtrick.bjoern.base;
 
 import de.mehtrick.bjoern.parser.reader.BjoernFileExtensions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.FilenameFilter;
@@ -11,11 +13,14 @@ import java.io.FilenameFilter;
  * {@link de.mehtrick.bjoern.parser.reader.BjoernFileExtensions}
  */
 public class BjoernFileNameFilter implements FilenameFilter {
+	private static final Logger log = LoggerFactory.getLogger(BjoernFileNameFilter.class);
+
 
 	private static final String FILENAMEEXTENSION = "."+BjoernFileExtensions.zgr;
 
 	@Override
 	public boolean accept(File dir, String name) {
+		log.info(name);
 		String lowercaseName = name.toLowerCase();
 		return (lowercaseName.endsWith(FILENAMEEXTENSION));
 	}
