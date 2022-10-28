dca075ad134fee4045d5b0cb309299fdfa970ad0
Patrick <p.lauhof@gmail.com>
2022-10-28 19:47:22 +0200

    #50 Fix

--- src/main/java/de/mehtrick/bjoern/doc/BjoernDocGenerator.java
+++ src/main/java/de/mehtrick/bjoern/doc/BjoernDocGenerator.java
@@ -5,45 +5,42 @@ import de.mehtrick.bjoern.base.BjoernGeneratorConfig;
 import de.mehtrick.bjoern.base.BjoernGeneratorConfigProvided;
 import de.mehtrick.bjoern.parser.modell.Bjoern;
 import freemarker.template.*;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.Map;
 
 /**
  * Generates documentation from bjoern files. The default ist ascii doc
  */
-public class BjoernDocGenerator extends BjoernGeneratorConfigProvided{
+public class BjoernDocGenerator extends BjoernGeneratorConfigProvided {
 
-	public BjoernDocGenerator(BjoernGeneratorConfig bjoernGeneratorConfig) {
-		super(bjoernGeneratorConfig);
-	}
+    public BjoernDocGenerator(BjoernGeneratorConfig bjoernGeneratorConfig) {
+        super(bjoernGeneratorConfig);
+    }
 
-	public void generate(Bjoern bjoern) throws IOException, TemplateException {
+    public void generate(Bjoern bjoern) throws IOException, TemplateException {
         Configuration cfg = createDefaultFreemarkerConfig();
         @SuppressWarnings("unchecked")
         //Converts the bjoern object into a map, so that freemarker can use the values of the file
-                Map<String, Object> convertValue = new ObjectMapper().convertValue(bjoern, Map.class);
-        Template temp = cfg.getTemplate(bjoernGeneratorConfig.getTemplate());
-        StringWriter stringWriter = new StringWriter();
-        temp.process(convertValue, stringWriter);
-        writeToFile(stringWriter.toString(), bjoern.getFilePath());
-        stringWriter.close();
+        Map<String, Object> convertValue = new ObjectMapper().convertValue(bjoern, Map.class);
+        Template template = cfg.getTemplate(bjoernGeneratorConfig.getTemplate(),bjoernGeneratorConfig.getEncoding().toString());
+        try (StringWriter stringWriter = new StringWriter()) {
+            template.process(convertValue, stringWriter);
+            writeToFile(stringWriter.toString(), bjoern.getFilePath());
+        }
     }
 
-	private void writeToFile(String text, String path) throws IOException {
+    private void writeToFile(String text, String path) throws IOException {
         cleanDocDir();
         String filename = FilenameUtils.removeExtension(new File(path).getName());
         File asciiDocFile = new File(
-                bjoernGeneratorConfig.getDocdir() + "/" + filename + "." + bjoernGeneratorConfig.getDocExtension());
-        try (PrintWriter out = new PrintWriter(asciiDocFile)) {
-            out.println(text);
-        }
-
+                bjoernGeneratorConfig.getDocdir() + File.separator + filename + "." + bjoernGeneratorConfig.getDocExtension());
+        FileUtils.write(asciiDocFile, text, bjoernGeneratorConfig.getEncoding());
     }
 
     private void cleanDocDir() {
